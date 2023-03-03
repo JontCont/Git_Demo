a81d77cdaa940c63fc4791eb8bf3af69620c1242
JontCont <52921053+JontCont@users.noreply.github.com>
2023-03-03 10:07:29 +0800

    Feature/#20230301 01 測試執行速度、記憶體 (#6)
    * Create TopicsAPI
    
    * 測試執行速度、記憶體
    
    * Load 、 Fill 、IEnumrable 取表

---
+++ TopicsAPI/Expansions/Efficiency.cs
@@ -0,0 +1,51 @@
+
+using System.Diagnostics;
+
+namespace TopicsAPI.Exceptions
+{
+    public class Efficiency
+    {
+        public static Action _action { get; set; }
+        public static Func<object> _func { get; set; }
+        public long CPU { get; set; }
+        public long Memory { get; set; }
+        public TimeSpan Time { get; set; }
+
+        public Efficiency(Action action)
+        {
+            _action = action;
+        }
+
+        public Efficiency(Func<object> func)
+        {
+            _func = func;
+        }
+
+
+        public object getEfficiency()
+        {
+            var stopwatch = new Stopwatch();
+            var process = Process.GetCurrentProcess();
+            var startTime = process.TotalProcessorTime;
+            stopwatch.Start();
+            if(_action != null) { _action.Invoke(); }
+            if(_func!=null) { _func.Invoke(); }
+            stopwatch.Stop();
+            var endTime = process.TotalProcessorTime;
+
+            var executionTime = stopwatch.Elapsed;
+            long executionMemory = process.PrivateMemorySize64 / 1024 / 1024;
+            long executionCpu = (long)((endTime - startTime).Ticks / stopwatch.Elapsed.Ticks * 100);
+            return new 
+            {
+                Time = executionTime,
+                Memory = executionMemory,
+                //CPU = executionCpu,
+            };
+        }
+
+
+   
+
+    }
+}
---
+++ TopicsAPI/Helpers/ConfigHelper.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+
+public class ConfigHelper
+{
+    /// <summary>
+    /// 取得 ConnectionStrings 底下資料
+    /// </summary>
+    /// <param name="name"></param>
+    /// <returns></returns>
+    public static string GetConnectionString(string name)
+    {
+        IConfiguration config = GetConfiguration();
+        return config.GetConnectionString(name);
+    }
+
+    public static IConfiguration GetConfiguration()
+    {
+        IConfiguration config = new ConfigurationBuilder()
+                      .SetBasePath(Directory.GetCurrentDirectory())
+                      .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                      .Build();
+
+        return config;
+    }
+
+}
